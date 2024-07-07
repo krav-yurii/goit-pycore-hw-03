f4ce7a076b2638ef84ff0709c8aafe0d4541314f
Yurii Kravchecko <yuriy@hubmee.com>
2024-07-07 17:03:01 +0300

    feature: added first task.

--- first.py
+++ first.py
@@ -1,1 +1,14 @@
-print('hello world')
+from datetime import datetime
+
+def get_days_from_today(date):
+
+    target_date = datetime.strptime(date, '%Y-%m-%d')
+    
+    today_date = datetime.now()
+
+    difference = today_date - target_date
+
+    return difference.days
+
+
+print(get_days_from_today('2024-07-06')) 
